726734d5973467018ea8e347ada3b5dcfb7ebbd0
Suraj Kashyap <suraj.kashyap370@webkul.in>
2024-12-05 13:38:55 +0530

    remove: Remove the language switcher package.

--- plugins/webkul/support/app/SupportServiceProvider.php
+++ plugins/webkul/support/app/SupportServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Webkul\Support;
 
-use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\Facades\Gate;
 use Livewire\Livewire;
 use Spatie\Permission\Models\Role;
@@ -32,12 +31,6 @@ class SupportServiceProvider extends PackageServiceProvider
         Livewire::component('accept-invitation', AcceptInvitation::class);
 
         Gate::policy(Role::class, RolePolicy::class);
-
-        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
-            $switch
-                ->locales(['ar', 'en', 'fr'])
-                ->circular();
-        });
     }
 
     public function packageRegistered(): void
