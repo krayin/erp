e73d7b180c2c64237c8524b652165ab9e91e3ba5
Suraj Kashyap <suraj.kashyap370@webkul.in>
2024-12-06 16:36:44 +0530

    Add tabs filters.

--- plugins/webkul/security/app/Filament/Resources/CompanyResource/Pages/ListCompanies.php
+++ plugins/webkul/security/app/Filament/Resources/CompanyResource/Pages/ListCompanies.php
@@ -3,8 +3,10 @@
 namespace Webkul\Security\Filament\Resources\CompanyResource\Pages;
 
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 use Webkul\Security\Filament\Resources\CompanyResource;
+use Webkul\Security\Models\Company;
 use Webkul\TableViews\Filament\Traits\HasTableViews;
 
 class ListCompanies extends ListRecords
@@ -17,6 +19,19 @@ class ListCompanies extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('All Companies')
+                ->badge(Company::count()),
+            'archived' => Tab::make('Archived')
+                ->badge(Company::onlyTrashed()->count())
+                ->modifyQueryUsing(function ($query) {
+                    return $query->onlyTrashed();
+                }),
         ];
     }
 }
