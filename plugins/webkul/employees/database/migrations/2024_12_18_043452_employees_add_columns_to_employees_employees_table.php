789f24dd52be01c09f1fd5a76ebe59c2094b8124
Suraj Kashyap <suraj.kashyap370@webkul.in>
2024-12-18 12:07:14 +0530

    Update employee migration.

--- plugins/webkul/employees/database/migrations/2024_12_18_043452_employees_add_columns_to_employees_employees_table.php
+++ plugins/webkul/employees/database/migrations/2024_12_18_043452_employees_add_columns_to_employees_employees_table.php
@@ -12,13 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('employees_employees', function (Blueprint $table) {
-            $table->string('tz')->nullable()->comment('Employee Timezone');
-            $table->string('work_permit')->nullable()->comment('Work permit document');
-            $table->unsignedBigInteger('address_id')->nullable()->comment('Company address id');
-            $table->unsignedBigInteger('leave_manager_id')->nullable()->comment('Leave manager id');
+            $table->string('tz')->nullable()->comment('Employee Timezone')->after('coach_id');
+            $table->string('work_permit')->nullable()->comment('Work permit document')->after('tz');
+            $table->unsignedBigInteger('address_id')->nullable()->comment('Company address ID')->after('work_permit');
+            $table->unsignedBigInteger('leave_manager_id')->nullable()->comment('Leave manager ID')->after('address_id');
 
-            $table->foreign('leave_manager_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('address_id')->references('id')->on('company_addresses')->onDelete('cascade');
+            $table->foreign('address_id')
+                ->references('id')
+                ->on('company_addresses')
+                ->onDelete('cascade');
+
+            $table->foreign('leave_manager_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
@@ -28,10 +35,15 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('employees_employees', function (Blueprint $table) {
-            $table->dropForeign(['leave_manager_id']);
             $table->dropForeign(['address_id']);
+            $table->dropForeign(['leave_manager_id']);
 
-            $table->dropColumn(['leave_manager_id', 'address_id', 'work_permit', 'tz']);
+            $table->dropColumn([
+                'tz',
+                'work_permit',
+                'address_id',
+                'leave_manager_id',
+            ]);
         });
     }
 };
