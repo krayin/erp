9bfdae5a8bd4200cdcbf37e58f1084575b85670e
Suraj Kashyap <suraj.kashyap370@webkul.in>
2024-12-23 15:17:52 +0530

    refactor or remove unused file.

--- plugins/webkul/chatter/app/Filament/Widgets/ChatterWidget.php
+++ plugins/webkul/chatter/app/Filament/Widgets/ChatterWidget.php
@@ -8,16 +8,12 @@ class ChatterWidget extends Widget
 {
     protected static string $view = 'chatter::filament.widgets.chatter';
 
-    // Set this to true to make it span the full width
     protected int|string|array $columnSpan = 'full';
 
-    // Make the record a public property that can be set
     public $record = null;
 
-    // Make it appear in the footer
     protected static string $type = 'footer';
 
-    // Add a method to set the record
     public function mount($record = null)
     {
         $this->record = $record;
@@ -25,10 +21,9 @@ class ChatterWidget extends Widget
 
     public static function canView(): bool
     {
-        return true; // Enable the widget
+        return true;
     }
 
-    // Optional: Add a method to get the record in your view
     public function getRecord()
     {
         return $this->record;
