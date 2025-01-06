f3eef5f28ec44e26b60fda723aeccb5550fdc0e5
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-01-06 15:08:00 +0530

    refactor: update follower mail layout to use new email component structure

--- plugins/webkul/chatter/resources/views/mail/follower-mail.blade.php
+++ plugins/webkul/chatter/resources/views/mail/follower-mail.blade.php
@@ -1,88 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            line-height: 1.6;
-            color: #333;
-            margin: 0;
-            padding: 20px;
-        }
-
-        .breadcrumb {
-            font-size: 14px;
-            margin-bottom: 20px;
-            border-bottom: 1px solid rgb(204, 204, 204);
-            padding-bottom: 10px;
-            display: flex;
-            align-items: center;
-        }
-
-        .breadcrumb p {
-            font-weight: bold;
-            margin: 0;
-            margin-left: 10px;
-        }
-
-        .view-button {
-            display: inline-block;
-            padding: 5px 10px;
-            font-size: 12px;
-            font-weight: bold;
-            color: #fff;
-            background-color: #007bff;
-            border-radius: 3px;
-            text-decoration: none;
-        }
-
-        .view-button:hover {
-            background-color: #0056b3;
-        }
-
-        .notification {
-            margin: 20px 0;
-            color: #555;
-            font-size: 13px;
-        }
-
-        .company-info {
-            font-size: 13px;
-            color: #666;
-            border-top: 1px solid rgb(204, 204, 204);
-            padding-top: 10px;
-        }
-
-        .company-name {
-            font-weight: bold;
-            margin-bottom: 5px;
-        }
-
-        .company-details {
-            margin: 0;
-        }
-
-        .powered-by {
-            margin-top: 10px;
-            font-size: 12px;
-            color: #999;
-        }
-
-        .powered-by a {
-            color: #666;
-            text-decoration: none;
-        }
-
-        .powered-by a:hover {
-            text-decoration: underline;
-        }
-    </style>
-</head>
-
-<body>
+<x-support::emails.layout>
     <div class="breadcrumb">
         <a
             href="{{ $payload['record_url'] }}"
@@ -114,10 +30,59 @@
             </p>
         </div>
     @endisset
+</x-support::emails.layout>
 
-    <div class="powered-by">
-        {{ __('chatter::views/mail/follower-mail.powered-by') }} <a href="#">{{ config('app.name') }}</a>
-    </div>
-</body>
+<style>
+    .breadcrumb {
+        font-size: 14px;
+        margin-bottom: 20px;
+        border-bottom: 1px solid rgb(204, 204, 204);
+        padding-bottom: 10px;
+        display: flex;
+        align-items: center;
+    }
 
-</html>
+    .breadcrumb p {
+        font-weight: bold;
+        margin: 0;
+        margin-left: 10px;
+    }
+
+    .view-button {
+        display: inline-block;
+        padding: 5px 10px;
+        font-size: 12px;
+        font-weight: bold;
+        color: #fff;
+        background-color: #007bff;
+        border-radius: 3px;
+        text-decoration: none;
+    }
+
+    .view-button:hover {
+        background-color: #0056b3;
+    }
+
+    .notification {
+        margin: 20px 0;
+        color: #555;
+        font-size: 13px;
+    }
+
+
+    .company-info {
+        font-size: 13px;
+        color: #666;
+        border-top: 1px solid rgb(204, 204, 204);
+        padding-top: 10px;
+    }
+
+    .company-name {
+        font-weight: bold;
+        margin-bottom: 5px;
+    }
+
+    .company-details {
+        margin: 0;
+    }
+</style>
