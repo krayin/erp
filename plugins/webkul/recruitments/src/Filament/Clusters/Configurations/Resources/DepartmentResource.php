6ce073f8ecf2bbf2ec4093f05307d3b21e7d8242
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-01-09 11:38:41 +0530

    Refactor Department, EmploymentType, and SkillType resources to extend base classes and simplify code structure

--- plugins/webkul/recruitments/src/Filament/Clusters/Configurations/Resources/DepartmentResource.php
+++ plugins/webkul/recruitments/src/Filament/Clusters/Configurations/Resources/DepartmentResource.php
@@ -4,64 +4,20 @@ namespace Webkul\Recruitment\Filament\Clusters\Configurations\Resources;
 
 use Webkul\Recruitment\Filament\Clusters\Configurations;
 use Webkul\Recruitment\Filament\Clusters\Configurations\Resources\DepartmentResource\Pages;
-use Filament\Forms\Form;
-use Filament\Infolists\Infolist;
-use Filament\Resources\Resource;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Model;
-use Webkul\Employee\Models\Department;
 use Webkul\Employee\Filament\Resources\DepartmentResource as BaseDepartmentResource;
 
-class DepartmentResource extends Resource
+class DepartmentResource extends BaseDepartmentResource
 {
-    protected static ?string $model = Department::class;
-
-    protected static ?string $navigationIcon = 'heroicon-o-building-office-2';
-
     protected static ?string $cluster = Configurations::class;
-
-    public static function getModelLabel(): string
-    {
-        return BaseDepartmentResource::getModelLabel();
-    }
 
     public static function getNavigationGroup(): string
     {
         return __('recruitments::filament/clusters/configurations/resources/department.navigation.group');
     }
 
-    public static function getNavigationLabel(): string
+    public static function getSlug(): string
     {
-        return BaseDepartmentResource::getNavigationLabel();
-    }
-
-    public static function getGloballySearchableAttributes(): array
-    {
-        return BaseDepartmentResource::getGloballySearchableAttributes();
-    }
-
-    public static function getGlobalSearchResultDetails(Model $record): array
-    {
-        return BaseDepartmentResource::getGlobalSearchResultDetails($record);
-    }
-
-    public static function form(Form $form): Form
-    {
-        return BaseDepartmentResource::form($form);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return BaseDepartmentResource::table($table)
-        ->contentGrid([
-            'md' => 3,
-            'xl' => 3,
-        ]);
-    }
-
-    public static function infolist(Infolist $infolist): Infolist
-    {
-        return BaseDepartmentResource::infolist($infolist);
+        return 'departments';
     }
 
     public static function getPages(): array
--- plugins/webkul/recruitments/src/Filament/Clusters/Configurations/Resources/EmploymentTypeResource/Pages/ListEmploymentTypes.php
+++ plugins/webkul/recruitments/src/Filament/Clusters/Configurations/Resources/EmploymentTypeResource/Pages/ListEmploymentTypes.php
@@ -3,22 +3,9 @@
 namespace Webkul\Recruitment\Filament\Clusters\Configurations\Resources\EmploymentTypeResource\Pages;
 
 use Webkul\Recruitment\Filament\Clusters\Configurations\Resources\EmploymentTypeResource;
-use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
+use Webkul\Employee\Filament\Clusters\Configurations\Resources\EmploymentTypeResource\Pages\ListEmploymentTypes as BaseListEmploymentTypes;
 
-class ListEmploymentTypes extends ListRecords
+class ListEmploymentTypes extends BaseListEmploymentTypes
 {
     protected static string $resource = EmploymentTypeResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make()
-                ->label(__('recruitments::filament/clusters/configurations/resources/employment-type/pages/list-employment-type.header-actions.create.label'))
-                ->icon('heroicon-o-plus-circle')
-                ->mutateFormDataUsing(function ($data) {
-                    return $data;
-                }),
-        ];
-    }
 }
