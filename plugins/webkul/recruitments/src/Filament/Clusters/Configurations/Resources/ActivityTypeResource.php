faf1284ba9ec381566a67c6bb8c4fc2a60a7c956
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-01-09 12:41:09 +0530

    Add ActivityType resource with CRUD pages and localization support

---
+++ plugins/webkul/recruitments/src/Filament/Clusters/Configurations/Resources/ActivityTypeResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Webkul\Recruitment\Filament\Clusters\Configurations\Resources;
+
+use Webkul\Recruitment\Filament\Clusters\Configurations;
+use Webkul\Recruitment\Filament\Clusters\Configurations\Resources\ActivityTypeResource\Pages;
+use Webkul\Support\Filament\Resources\ActivityTypeResource as BaseActivityTypeResource;
+
+class ActivityTypeResource extends BaseActivityTypeResource
+{
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $cluster = Configurations::class;
+
+    protected static bool $shouldRegisterNavigation = true;
+
+    public static function getNavigationGroup(): string
+    {
+        return __('recruitments::filament/clusters/configurations/resources/activity-type.navigation.group');
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index'  => Pages\ListActivityTypes::route('/'),
+            'create' => Pages\CreateActivityType::route('/create'),
+            'edit'   => Pages\EditActivityType::route('/{record}/edit'),
+            'edit'   => Pages\ViewActivityType::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ plugins/webkul/recruitments/src/Filament/Clusters/Configurations/Resources/ActivityTypeResource/Pages/EditActivityType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Webkul\Recruitment\Filament\Clusters\Configurations\Resources\ActivityTypeResource\Pages;
+
+use Webkul\Recruitment\Filament\Clusters\Configurations\Resources\ActivityTypeResource;
+use Webkul\Support\Filament\Resources\ActivityTypeResource\Pages\EditActivityType as BaseEditActivityType;
+
+class EditActivityType extends BaseEditActivityType
+{
+    protected static string $resource = ActivityTypeResource::class;
+}
